fd61803d35c669c7e0ad3b8b39cbc4d9d6e38f76
Dror Handler <hdror507@gmail.com>
2024-07-03 00:39:27 +0300

    check token has expiry

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const { createServer } = require('node:http');
 const jwt = require('jsonwebtoken');
 const port = 3006;
 const secretKey = 'xR1jlcGiyWHsR2JoSEOAsg8mMFaLTmStQLdUHqu6CRu4ITA3FfScJAhYSplFEAxb'
-
+// const token = jwt.sign({ userId: '123' }, secretKey, { expiresIn: '1m' });
+// console.log(token);
 const server = createServer((req, res) => {
     if (req.method === 'POST' && req.url === '/calculate') {
         let reqBody = ''
@@ -19,8 +20,7 @@ const server = createServer((req, res) => {
                 return res.end(JSON.stringify({ error: 'Data error' }));
             }
 
-
-            const { num1, num2 } = nums
+            let { num1, num2 } = nums
             const operation = req.headers['operation-type'];
             let reqToken = req.headers['authorization'];
 
@@ -30,11 +30,13 @@ const server = createServer((req, res) => {
                 return res.end(JSON.stringify({ error: 'Missing required fields' }));
             }
             // Check that num1 and num2 are intiger type
-            if (isNaN(+num1) || isNaN(+num2)) {
+            if (!isNaN(+num1) && !isNaN(+num2)) {
+                num1 = +num1
+                num2 = +num2
+            } else {
                 res.writeHeader(400, { 'Content-type': 'application/json' })
                 return res.end(JSON.stringify({ error: 'Must be a number' }));
             }
-
 
             if (!reqToken) {
                 res.writeHead(403, { 'Content-Type': 'application/json' });
@@ -45,13 +47,45 @@ const server = createServer((req, res) => {
             reqToken = reqToken.split(' ')[1]
 
             // Check if the received token is a match to the secret key 
-            jwt.verify(reqToken, secretKey, (err, decoded) => {
-                // console.log(token,'&&&(&(&(&');
+            jwt.verify(reqToken, secretKey, (err, decode) => {
+                if (!decode) {
+                    console.log('Token expired');
+                    res.writeHead(500, { 'Content-Type': 'application/json' });
+                    return res.end(JSON.stringify({ error: 'Token expired' }));
+
+                } else if (!decode.exp) {
+                    console.log('decode', decode);
+                    res.writeHead(500, { 'Content-Type': 'application/json' });
+                    return res.end(JSON.stringify({ error: 'Token invalid' }));
+                }
+
                 if (err) {
                     console.log(err);
                     res.writeHead(500, { 'Content-Type': 'application/json' });
                     return res.end(JSON.stringify({ error: 'Failed to authenticate token' }));
-                } else console.log(decoded);
+                }
+
+                let result;
+                switch (operation) {
+                    case 'add':
+                        result = num1 + num2;
+                        break;
+                    case 'subtract':
+                        result = num1 - num2;
+                        break;
+                    case 'multiply':
+                        result = num1 * num2;
+                        break;
+                    case 'divide':
+                        result = num1 / num2;
+                        break;
+                    default:
+                        res.writeHead(400, { 'Content-Type': 'application/json' });
+                        return res.end(JSON.stringify({ error: 'Invalid operation type' }));
+                }
+                res.writeHead(200, { 'Content-Type': 'application/json' })
+                res.end(JSON.stringify({ result }))
+
             })
         })
     }
