4c929f5cc0e9f0974c1d4d31a9eb67d47009ef58
Dror Handler <hdror507@gmail.com>
2024-07-02 20:10:13 +0300

    add checking for method and url

--- server.js
+++ server.js
@@ -3,11 +3,23 @@ const { createServer } = require('node:http');
 const port = 3006;
 
 const server = createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+    if (req.method === 'POST' && req.url === '/calculate') {
+        let reqBody
+        req.on('data', data => {
+            reqBody += data.toString()
+        })
+
+    }
+
+
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello World');
 });
 
+
+
+
 server.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}/`);
+    console.log(`Server running at http://localhost:${port}/`);
 });
