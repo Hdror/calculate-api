d0d7cd8466b2d5df8a43a2c0182a315088416844
Dror Handler <hdror507@gmail.com>
2024-07-02 23:25:40 +0300

    implement jwt

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const { createServer } = require('node:http');
-
+const jwt = require('jsonwebtoken');
 const port = 3006;
+const secretKey = 'xR1jlcGiyWHsR2JoSEOAsg8mMFaLTmStQLdUHqu6CRu4ITA3FfScJAhYSplFEAxb'
 
 const server = createServer((req, res) => {
     if (req.method === 'POST' && req.url === '/calculate') {
@@ -18,20 +19,40 @@ const server = createServer((req, res) => {
                 return res.end(JSON.stringify({ error: 'Data error' }));
             }
 
-            const operation = req.headers['operation-type'];
-            // const token = req.headers['authorization'];
-            const { num1, num2 } = nums
 
+            const { num1, num2 } = nums
+            const operation = req.headers['operation-type'];
+            let reqToken = req.headers['authorization'];
+
+            // Check if any of the props are missing
             if (!num1 || !num2 || !operation) {
                 res.writeHeader(400, { 'Content-type': 'application/json' })
                 return res.end(JSON.stringify({ error: 'Missing required fields' }));
             }
-
+            // Check that num1 and num2 are intiger type
             if (isNaN(+num1) || isNaN(+num2)) {
                 res.writeHeader(400, { 'Content-type': 'application/json' })
                 return res.end(JSON.stringify({ error: 'Must be a number' }));
             }
 
+
+            if (!reqToken) {
+                res.writeHead(403, { 'Content-Type': 'application/json' });
+                return res.end(JSON.stringify({ error: 'No token provided' }));
+            }
+
+            // Extract JWT from Authorization header
+            reqToken = reqToken.split(' ')[1]
+
+            // Check if the received token is a match to the secret key 
+            jwt.verify(reqToken, secretKey, (err, decoded) => {
+                // console.log(token,'&&&(&(&(&');
+                if (err) {
+                    console.log(err);
+                    res.writeHead(500, { 'Content-Type': 'application/json' });
+                    return res.end(JSON.stringify({ error: 'Failed to authenticate token' }));
+                } else console.log(decoded);
+            })
         })
     }
 
